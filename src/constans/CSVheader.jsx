9dd21f3137aad49131d5be489948b723a2c69ced
tjrmswo <93309061+tjrmswo@users.noreply.github.com>
2024-02-14 17:03:27 +0900

    feat: 엑셀 파일 다운로드(유저정보,답변&삽화)

---
+++ src/constans/CSVheader.jsx
@@ -0,0 +1,7 @@
+export const headers = [
+  { label: "번호", key: "userId" },
+  { label: "이름", key: "userName" },
+  { label: "전화번호", key: "userPhoneNumber" },
+  { label: "주소", key: "userAddress" },
+  { label: "닉네임", key: "userNickname" },
+];
