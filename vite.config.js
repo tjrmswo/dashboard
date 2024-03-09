6d20f742c2c0b9cbbebe7f207d3c07e138dc36ef
tjrmswo <93309061+tjrmswo@users.noreply.github.com>
2024-03-09 22:51:31 +0900

    bugfix: Mixed Content 에러 해결5

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,18 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
+      // 경로가 "/api" 로 시작하는 요청을 대상으로 proxy 설정
       "/api": {
-        target: "https://inklnk.kro.kr:8085/",
+        // 요청 전달 대상 서버 주소 설정
+        target: "${import.meta.env.VITE_API_ADDRESS}",
+        // 요청 헤더 host 필드 값을 대상 서버의 호스트 이름으로  변경
         changeOrigin: true,
-        secure: true,
+        // 요청 경로에서 '/api' 제거
+        rewrite: (path) => path.replace(/^\/api/, ""),
+        // SSL 인증서 검증 무시
+        secure: false,
+        // WebSocket 프로토콜 사용
+        ws: true,
       },
     },
   },
