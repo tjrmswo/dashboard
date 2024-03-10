d5c26e31ffd4c1470bdacbd80d966345fd4fce17
tjrmswo <93309061+tjrmswo@users.noreply.github.com>
2024-03-10 16:19:57 +0900

    bugfix: 오류 해결 2일차 6

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       // 경로가 "/api" 로 시작하는 요청을 대상으로 proxy 설정
       "/api": {
         // 요청 전달 대상 서버 주소 설정
-        target: "http://inklnk.kro.kr:8085",
+        target: "http://inklnk.kro.kr:8085/",
         // 요청 헤더 host 필드 값을 대상 서버의 호스트 이름으로  변경
         changeOrigin: true,
         // 요청 경로에서 '/api' 제거
